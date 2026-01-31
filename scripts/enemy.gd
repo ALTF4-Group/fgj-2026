4f05e0ddf6b5ef0d27516362863a7849c0d0ea0a
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2026-02-01 00:35:20 +0200

    #49 Working xp bar

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var player = Node2D
 @export var health = 1
 @export var damage = 1
+@export var experience = 5
 var speed = 500
 
 func _ready() -> void:
@@ -17,5 +18,6 @@ func _physics_process(delta) -> void:
 func take_damage(damage: int):
 	health -= damage
 	if health <= 0:
+		Global.player.on_kill(experience)
 		Global.enemy_counter -= 1
 		queue_free()
---
+++ scripts/experience_handler.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+signal level_up(level: int)
+
+@export var level = 0
+@export var base_xp = 100
+@export var growth = 1.2
+@export var current_xp = 0
+var to_next_level = 100
+
+func _get_next_level_requirement() -> int:
+	return floori(base_xp * growth ** level)
+
+func _update_bar_on_level_up() -> void:
+	$ProgressBar.value = current_xp
+	$ProgressBar.max_value = to_next_level
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_update_bar_on_level_up()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func gain_xp(amount: int) -> void:
+	current_xp += amount
+	if current_xp >= to_next_level:
+		current_xp -= to_next_level
+		level += 1
+		level_up.emit(level)
+		to_next_level = _get_next_level_requirement()
+		_update_bar_on_level_up()
+	else:
+		$ProgressBar.value = current_xp
