8cb32ac4fc8e03c8203dad7d915c900aada83e6c
Joona Pellinen <joonaaleks@gmail.com>
2026-01-31 09:49:11 +0200

    Added test level and placeholder player character

---
+++ test_level.gd
@@ -0,0 +1,8 @@
+extends Node2D
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
