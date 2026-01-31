460f5baffc3a2c5d114fce5ff89870f237d5e5ef
Elias Ryökäs <97228240+Noppacase22@users.noreply.github.com>
2026-01-31 16:04:00 +0200

    Apply suggestions from code review
    added delta time

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,12 +3,12 @@ extends CharacterBody2D
 @export var player = Node2D
 @export var hp = 1
 @export var damage = 1
-var speed = 100
+var speed = 6000
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta) -> void:
 	var direction = (player.position - position).normalized()
-	velocity = direction * speed
+	velocity = direction * speed * delta
 	move_and_slide()
