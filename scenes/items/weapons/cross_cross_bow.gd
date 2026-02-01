0d74f0fe18ce72b14a741f634e26941b009f24e9
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2026-02-01 09:57:13 +0200

    #54 Level up support for crossbow

--- scenes/items/weapons/cross_cross_bow.gd
+++ scenes/items/weapons/cross_cross_bow.gd
@@ -2,30 +2,94 @@ extends Node2D
 
 var crossbow_scene: PackedScene = preload("res://scenes/items/weapons/crossbow.tscn")
 @export var shoot_interval := 1
-@export var rotation_speed := 1.0
+@export var rotation_speed := 0
 @export var number_of_bows = 4
+@export var level := 0
 var list_of_bows = []
 
-func _ready() -> void:
+const levels = [
+	{
+		"shoot_interval": 1,
+		"rotation_speed": 0,
+		"number_of_bows": 4,
+		"damage": 1,
+		"pierce": 1,
+		"knockback": 1,
+		"projectile_speed": 200
+	},
+	{
+		"shoot_interval": 0.9,
+		"rotation_speed": 0,
+		"number_of_bows": 4,
+		"damage": 1,
+		"pierce": 2,
+		"knockback": 1,
+		"projectile_speed": 250
+	},
+	{
+		"shoot_interval": 0.8,
+		"rotation_speed": 0,
+		"number_of_bows": 4,
+		"damage": 2,
+		"pierce": 2,
+		"knockback": 2,
+		"projectile_speed": 300
+	},
+	{
+		"shoot_interval": 0.7,
+		"rotation_speed": 0,
+		"number_of_bows": 4,
+		"damage": 3,
+		"pierce": 3,
+		"knockback": 3,
+		"projectile_speed": 350
+	},
+	{
+		"shoot_interval": 0.7,
+		"rotation_speed": 1,
+		"number_of_bows": 8,
+		"damage": 3,
+		"pierce": 3,
+		"knockback": 3,
+		"projectile_speed": 400
+	}
+]
+
+func _set_attributes_from_level() -> void:
 	var current_degree = 0
+	shoot_interval = levels[level]["shoot_interval"]
+	rotation_speed = levels[level]["rotation_speed"]
+	number_of_bows = levels[level]["number_of_bows"]
 	for i in range(number_of_bows):
 		var crossbow = crossbow_scene.instantiate()
 		crossbow.rotation = current_degree
+		crossbow.damage = levels[level]["damage"]
+		crossbow.pierce = levels[level]["pierce"]
+		crossbow.knockback = levels[level]["knockback"]
+		crossbow.projectile_speed = levels[level]["projectile_speed"]
 		current_degree += PI/number_of_bows*2
 		list_of_bows.append(crossbow)
 		$".".add_child(crossbow)
-	pass 
 
+func _ready() -> void:
+	_set_attributes_from_level()
 
 
 func _process(delta: float) -> void:
-	#var player_pos = Global.player.global_position
 	$".".rotate(rotation_speed * delta)
-	pass
 
 
 func _on_timer_timeout() -> void:
 	for bow in list_of_bows:
 		bow.shoot()
 	$Timer.wait_time = shoot_interval
-	pass # Replace with function body.
+
+func level_up() -> void:
+	if level + 1 >= len(levels):
+		return
+	for bow in list_of_bows:
+		bow.queue_free()
+	list_of_bows = []
+	level += 1
+	_set_attributes_from_level()
+	
--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -4,13 +4,17 @@ var source: Object
 var direction: Vector2
 var projectile_speed: int
 var bullet_damage: int
+var pierce: int
+var knockback: int
 
 
-func create(pos:Vector2, dir: Vector2, speed: int, damage: int):
+func create(pos:Vector2, dir: Vector2, speed: int, damage: int, initial_pierce: int = 1, initial_knockback: int = 1):
 	direction = dir
 	position = pos
 	projectile_speed = speed
 	bullet_damage = damage
+	pierce = initial_pierce
+	knockback = initial_knockback
 
 
 func _physics_process(delta: float) -> void:
@@ -22,7 +26,10 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
+	if pierce <= 0:
+		return
 	if body.is_in_group("enemies"):
-		print("Hit!")
-		#body.health -= damage
-		#print(body.health)
+		pierce -= 1
+		body.take_damage(bullet_damage)
+		if pierce <= 0:
+			queue_free()
