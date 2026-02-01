d6c7aa59ae7a5ce38d441c1973e33bc729976752
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2026-02-01 10:05:51 +0200

    #54 Level up support for boomerang

--- scenes/items/weapons/boomerang.gd
+++ scenes/items/weapons/boomerang.gd
@@ -8,7 +8,42 @@ enum State { INACTIVE, THROW, RETURN }
 var direction : Vector2
 @export var acceleration : float = 250.0
 @export var max_speed : float = 200.0
+@export var knockback : float = 1
+@export var level : int = 0
 var state
+
+const levels = [
+	{
+		"damage": 2,
+		"knockback": 1,
+		"max_speed": 200
+	},
+	{
+		"damage": 4,
+		"knockback": 2,
+		"max_speed": 250
+	},
+	{
+		"damage": 6,
+		"knockback": 3,
+		"max_speed": 300
+	},
+	{
+		"damage": 8,
+		"knockback": 4,
+		"max_speed": 350
+	},
+	{
+		"damage": 10,
+		"knockback": 5,
+		"max_speed": 400
+	}
+]
+
+func _set_stats_from_level() -> void:
+	damage = levels[level]["damage"]
+	knockback = levels[level]["knockback"]
+	max_speed = levels[level]["max_speed"]
 
 func _physics_process( delta: float ) -> void:
 	$".".rotate(-rotation_speed * delta)
@@ -28,6 +63,9 @@ func _physics_process( delta: float ) -> void:
 	var speed_ratio = speed / max_speed
 	pass
 
+func _ready() -> void:
+	_set_stats_from_level()
+
 func throw( throw_direction : Vector2 ) -> void:
 	direction = throw_direction
 	speed = max_speed
@@ -46,3 +84,9 @@ func _on_tree_entered() -> void:
 	var throw_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	print(throw_direction)
 	throw(throw_direction)
+
+func level_up() -> void:
+	if level + 1 >= len(levels):
+		return
+	level += 1
+	_set_stats_from_level()
